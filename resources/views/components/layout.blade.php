cb0af6ec00c8a81479463848c46e55538f8d3e90
KauanAmorim <kauanamorim88@gmail.com>
2024-04-21 21:56:36 -0300

    feat: adicionando página de adição de séries na lista e mexendo com bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ $title }}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>{{ $title }}</title>
 </head>
+
 <body>
-  <h1>{{ $title }} </h1>
-  {{ $slot }}
+    <div class="container">
+        <h1>{{ $title }} </h1>
+        {{ $slot }}
+    </div>
 </body>
+
 </html>

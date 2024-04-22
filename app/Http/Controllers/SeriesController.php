b892b3a1460c7aa8bf42c7847f0e937013719916
KauanAmorim <kauanamorim88@gmail.com>
2024-04-22 00:13:30 -0300

    feat: adicionando funcionalidade para exclusão de item da lista de series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::all();
-        return view('series.index', compact('series'));
+        $successMessage = $request->session()->get('success.message');
+        return view('series.index')
+            ->with('series', $series)
+            ->with('successMessage', $successMessage);
     }
 
     public function create()
@@ -22,5 +25,12 @@ class SeriesController extends Controller
     {
         Serie::create($request->all());
         return to_route('series.index');
+    }
+
+    public function destroy(Serie $series, Request $request)
+    {
+        $series->delete();
+        return to_route('series.index')
+            ->with('success.message', "Série '{$series->nome}' removida com sucesso");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/series', SeriesController::class);
+Route::resource('/series', SeriesController::class)->except('show');
